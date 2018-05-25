0938730872d3fba4c2f514258fed31a1900d504f
DESKTOP-JTP554M\User <nikolayev.sergii@gmail.com>
2018-05-25 13:13:26 +0300

    254 lesson 1 | Installing Redux and React Redux

---
+++ src/store/actions.js
@@ -0,0 +1,4 @@
+/** first we looked at BurgerBuilder.js to see what methods we use there (it's addIngredientHandler and removeIngredientHandler) and then we start to create actions named like this*/
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    /** we copy state from BurgerBuilder.js */
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = ( state = initialState, action ) => {
+    /** it's reducer itself */
+
+};
+
+export default reducer;
